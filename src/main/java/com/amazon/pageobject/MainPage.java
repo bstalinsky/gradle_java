0bb115b41863675be588bb38fdb05c2b0d6a7e86
bohdan.stalinskyi <bstalinsky@gmail.com>
2022-11-21 00:50:58 +0200

    gitignore commit

--- src/main/java/com/amazon/pageobject/MainPage.java
+++ src/main/java/com/amazon/pageobject/MainPage.java
@@ -9,5 +9,16 @@ public class MainPage {
     private final By menuLangNavigate = By.xpath("//div[@id='nav-tools']//a//span[@class='icp-nav-link-inner']");
     private final By searchLine = By.xpath("//input[@id='twotabsearchtextbox']");
     private final By searchLineBut = By.xpath("//input[@id='nav-search-submit-button']");
+    private final By searchDropDownBox = By.xpath("//select[@id='searchDropdownBox']");
+    private final By amazonLogo = By.xpath("//a[@id='nav-logo-sprites']");
+
+    private final By navBarBut = By.xpath("//i[@class='hm-icon nav-sprite']");
+    private final By navBarMenuList = By.xpath("//ul[@class='hmenu hmenu-visible']//li");
+    private final By signInMenuBut = By.xpath("//span[@id='nav-link-accountList-nav-line-1']");
+    private final By signInBut = By.xpath("//div[@id='nav-al-signin']//div//a//span");
+    private final By yourAccountList = By.xpath("//*[@id='nav-al-your-account']//a");
+    private final By saleBut = By.xpath("//a[normalize-space()='Sell']");
+
+
 
 }
