fec8fc6e2b42ce0f6717a7eb220d99ea6b73f1e0
bohdan.stalinskyi <bstalinsky@gmail.com>
2022-12-07 10:12:28 +0200

    groups tests

--- src/test/java/tests/amazon/SignInTests.java
+++ src/test/java/tests/amazon/SignInTests.java
@@ -9,14 +9,14 @@ public class SignInTests extends BasicTest {
     public static String password = "Dsaewqxx!!";
     String ERROR_TXT = "There was a problem";
 
-    @Test(groups = {"positive"})
-    public void clickToSignInSignOut() throws InterruptedException {
+    @Test(groups = {"positive" , "smoke"}, priority = 2)
+    public void clickToSignInSignOut() {
          new MainPage(baseFunc)
                  .clickSignIn()
                  .signIn(email , password)
                  .signInSuccessful().moveToMenuNavBar().signOutBut().asserSignOut();
     }
-    @Test(groups = {"negative", "smoke"})
+    @Test(groups = {"negative", "smoke"}, priority = 1)
     public void invalidSignInEmail1()   {
        new MainPage(baseFunc).clickSignIn()
               .invalidEmailType("email")
